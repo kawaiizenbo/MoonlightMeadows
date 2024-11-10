72ec2869b80e2a6d92c106ed01d83334e6a8386f
kawaiizenbo <48113593+kawaiizenbo@users.noreply.github.com>
2024-11-09 19:10:46 -0700

    fix the shit that broke

--- src/main/java/me/kawaiizenbo/moonlight/module/settings/IndexSetting.java
+++ src/main/java/me/kawaiizenbo/moonlight/module/settings/IndexSetting.java
@@ -4,6 +4,7 @@ import me.kawaiizenbo.moonlight.Moonlight;
 import me.kawaiizenbo.moonlight.util.DrawUtils;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.Text;
 
 public class IndexSetting extends Setting
@@ -26,7 +27,7 @@ public class IndexSetting extends Setting
         drawContext.fill(x+96, y+5, x+190, y+19, Moonlight.THEME.border.getRGB());
         drawContext.fill(x+97, y+6, x+189, y+18, hovered(mouseX, mouseY) ? Moonlight.THEME.hover.getRGB() : Moonlight.THEME.background.getRGB());
         drawContext.drawText(textRenderer, elements[index], x+98, y+8, Moonlight.THEME.text.getRGB(), false);
-        drawContext.drawGuiTexture(DrawUtils.getThemedGUIIcon("updown", Moonlight.THEME.background), x+177, y+6, 12, 12);
+        drawContext.drawGuiTexture(RenderLayer::getGuiTextured, DrawUtils.getThemedGUIIcon("updown", Moonlight.THEME.background), x+177, y+6, 12, 12);
     }
 
     @Override
